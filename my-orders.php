9146313d04b2df01ba12ad2ddec6cdf6dd3553a6
xanotic <muhdnabil6699@gmail.com>
2025-07-05 20:47:27 +0800

    Update CSS link in my-orders.php to include cache-busting query parameter for improved loading performance.

--- my-orders.php
+++ my-orders.php
@@ -20,7 +20,7 @@ $orders = getCustomerOrders($conn, $_SESSION['user_id']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Orders - Cofeology</title>
     <link href="https://fonts.googleapis.com/css2?family=Monerrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
