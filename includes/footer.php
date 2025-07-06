fe72169599c4d35578f2fcb96b91ec2d0faa0d39
Dahlia <dahliawr3@gmail.com>
2025-07-06 17:31:10 +0800

    ejas

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,13 @@
         <div class="footer-content">
             <div class="footer-section">
                 <h3>Cofeology Café & Restaurant</h3>
-                <p>Cofeology Café & Restaurant is founded on the spirit to bring out the best of local produce has to offer. Its unique dishes with high standards of quality and freshness did not come with a hefty price tag.</p>
+                <p style="text-align: justify;">
+                    Cofeology Café & Restaurant is founded on the spirit to bring out the best of local produce has to offer. Its unique dishes with high standards of quality and freshness did not come with a hefty price tag.
+                </p>
             </div>
             <div class="footer-section">
                 <h3>Contact Us</h3>
-                <p><i class="fas fa-map-marker-alt"></i> Cofeology Grand Boulevard, 21 - 23, Jalan Bukit Beruang, Central Business District,, Malacca City, Malaysia, 75450</p>
+                <p><i class="fas fa-map-marker-alt"></i> Cofeology Grand Boulevard, 21 - 23, Jalan Bukit Beruang, Central Business District, 75450, Malacca City, Malaysia</p>
                 <p><i class="fas fa-phone"></i> +011-5277 7730</p>
                 <p><i class="fas fa-envelope"></i> info@cofeology.com</p>
             </div>
